6adbab1f5f06cbd65ea35ac587fedcca434bcb64
esteban1234 <tva_libra@hotmail.com>
2018-06-07 13:49:53 -0500

    PAGINA LISTA

---
+++ index.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>GLOBAL SOLUTION FCR</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <META NAME="Title" content="GLOBAL SOLUTION FCR">
+    <META NAME="Author" CONTENT="[http://globalsolutionfcr.com/]"/>
+    <META name="Description" content="Asesoría Fiscal, Legal, Auditorías, Precios de Transfer, Valuación de Empresas. Firma #1 · Tranquilidad y Confianza · Servicios de Excelencia."/>
+    <META NAME="Keywords" CONTENT="Asesoría Fiscal, Legal, Auditorías, Precios de Transfer, Valuación de Empresas. Firma #1 · Tranquilidad y Confianza · Servicios de Excelencia"/>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Khula|Montserrat|Roboto" rel="stylesheet">
+    <script src="js/general.js"></script>
+    <script src="js/fontawesome-all.js"></script>
+
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+    <div class="banner">
+      <div class="menu">
+            <nav class="pull-left">
+              <ul>
+                <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO</a>
+                <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+                <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">SERVICIOS</a>
+                <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+              </ul>
+            </nav>
+      
+            <figure class="logo pull-right">
+              <img src="img/fcr.png" alt="FCR" class="img-responsive">
+            </figure>
+      </div>
+
+      <p class="tit">La tranquilidad de contar con nuextra experiencia</p>
+      <center style="margin-top: 3%;">
+        <a data-scroll href="#CONTACTO" aria-pressed="true"><button class="btn btn-lg btn-secondary"><i class="fas fa-phone-volume"></i> CONTADORES PUBLICOS Y ABOGADOS</button></a>
+      </center>
+    </div>
+          
+
+    
+        
+     
+
+
+
+<section class="seccion2" id="NOSOTROS" style="padding: 5% 0% 5% 0%;">
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center" style="color: #1D1D3C;"><i class="fas fa-user-tie"></i> Quines somos. Un poco sobre nosotros</h1> <br><br>
+      <p class="texto">Iniciamos ofreciendo consultorías fiscales, fuimos agregando servicios y personal hasta el día de hoy donde hemos atendido a un sin fin de empresas. <br><br>
+
+      Con experiencia, seguimos con el compromiso intacto de garantizar a nuestros clientes servicios de excelencia siempre con los más altos estándares de calidad que rigen nuestra profesión.<br><br>
+
+      Logramos tranquilidad y confianza para nuestros clientes, empleando las técnicas que les permitan enfocar sus energías en el crecimiento y proyección de su negocio.<br><br>
+
+      Generamos soluciones integrales bajo los siguientes valores:</p>
+    </div> <br><br> 
+
+
+        <div class="row">
+          <div class="col-md-3 uno">
+            <div class="icono">
+              <span class="fa-stack fa-4x i">
+                <i class="fas fa-circle fa-stack-2x ii"></i>
+                <i class="fas fa-balance-scale fa-stack-1x fa-inverse iii"></i>
+              </span>
+            </div>
+            <h2 class="text-center">Ética</h2>
+          </div>
+          <div class="col-md-3 uno">
+            <div class="icono">
+              <span class="fa-stack fa-4x i">
+                <i class="fas fa-circle fa-stack-2x ii"></i>
+                <i class="fas fa-hands-helping fa-stack-1x fa-inverse iii"></i>
+              </span>
+            </div>
+            <h2 class="text-center">Honestidad</h2>
+          </div>
+          <div class="col-md-3 uno">
+            <div class="icono">
+              <span class="fa-stack fa-4x i">
+                <i class="fas fa-circle fa-stack-2x ii"></i>
+                <i class="fas fa-american-sign-language-interpreting fa-stack-1x fa-inverse iii"></i>
+              </span>
+            </div>
+            <h2 class="text-center">Calidad</h2>
+          </div>
+          <div class="col-md-3 uno">
+            <div class="icono">
+              <span class="fa-stack fa-4x i">
+                <i class="fas fa-circle fa-stack-2x ii"></i>
+                <i class="fas fa-users-cog fa-stack-1x fa-inverse iii"></i>
+              </span>
+            </div>
+            <h2 class="text-center">Eficiencia</h2>
+          </div>
+        </div>
+  </div>
+    
+</section>
+
+<section class="seccion1" id="SERVICIOS" style="padding: 5% 0%; background: #f5eef8;" >
+
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center" style="color: #1D1D3C;"><i class="fas fa-briefcase"></i> Servicios</h1> <br><br>
+      <p class="texto"></p>
+    </div> 
+
+    <div class="row">
+      <div class="col-md-12">
+        
+        <div role="tabpanel">
+          <ul class="nav nav-tabs" role="tablist">
+            <li  role="presentation" class="active"><a id="color" href="#FISCAL" aria-controls="FISCAL" data-toggle="tab" role="tab">FISCAL</a></li>
+            <!-- <li role="presentation"><a id="color" href="#LEGAL" aria-controls="LEGAL" data-toggle="tab" role="tab"></a></li> -->
+            <li role="presentation"><a id="color" href="#AUDITORIA" aria-controls="AUDITORIA" data-toggle="tab" role="tab">AUDITORIA</a></li>
+            <li role="presentation"><a id="color" href="#PRECIOS" aria-controls="PRECIOS" data-toggle="tab" role="tab">PRECIOS DE TRANSFERENCIA</a></li>
+            <li role="presentation"><a id="color" href="#VALUACION" aria-controls="VALUACION" data-toggle="tab" role="tab">VALUACION DE EMPRESAS</a></li>
+            <li role="presentation"><a id="color" href="#CONTABLE" aria-controls="CONTABLE" data-toggle="tab" role="tab">CONTABLE</a></li>
+            <li role="presentation"><a id="color" href="#TRABAJO" aria-controls="TRABAJO" data-toggle="tab" role="tab">ASESORIA DE TRABAJO</a></li>
+          </ul>
+
+          <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="FISCAL">
+              <h3>FISCAL</h3>
+              <p><i class="far fa-check-circle"></i> Asesoramiento general en materia fiscal. <br>
+
+                <i class="far fa-check-circle"></i> Declaraciones de IVA y Resumen Anual.<br>
+
+                <i class="far fa-check-circle"></i> Declaraciones de IRPF de trabajadores y actividades económicas y Resumen Anual.<br>
+
+                <i class="far fa-check-circle"></i> Declaraciones de IRPF de Arrendamientos.<br>
+
+                <i class="far fa-check-circle"></i> Impuesto de Sociedades: Declaración Anual y pagos fraccionados.<br>
+
+                <i class="far fa-check-circle"></i> Declaración anual de operaciones con terceras personas.<br>
+
+                <i class="far fa-check-circle"></i> Declaración anual de operaciones intracomunitarias.<br>
+
+                <i class="far fa-check-circle"></i> Asistencia y representación ante las administraciones públicas: inspecciones fiscales, requerimientos, recursos, aplazamientos, etc.<br>
+
+                <i class="far fa-check-circle"></i> Alta, Modificación de datos y Baja en el censo.<br>
+
+                <i class="far fa-check-circle"></i> Preparación y Tramitación de Impuestos ante la Administración Autónoma: Transmisiones patrimoniales, Actos Jurídicos Documentados, Sucesiones, etc.<br>
+
+                <i class="far fa-check-circle"></i> Haciendas locales.</p>
+            </div>
+            <!-- <div role="tabpanel" class="tab-pane" id="LEGAL">
+              <h3>LEGAL</h3>
+              <p>2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus eos, iure voluptates nam mollitia deleniti error fuga nobis soluta laudantium velit corrupti distinctio labore earum, iusto neque nisi dolores perspiciatis.</p>
+            </div> -->
+            <div role="tabpanel" class="tab-pane" id="AUDITORIA">
+              <h3>AUDITORIA</h3>
+              <p>
+                <i class="far fa-check-circle"></i> De estados financieros. <br>
+
+                <i class="far fa-check-circle"></i> Trabajos de atestiguamiento y servicios relacionados.<br>
+
+                <i class="far fa-check-circle"></i> Dictamen para efectos fiscales.<br>
+
+                <i class="far fa-check-circle"></i> Dictamen para efectos de IMSS e INFONAVIT.<br>
+
+                <i class="far fa-check-circle"></i> Conversión de estados financieros de NF mexicanas a US GAAP y a IFRS.<br>
+
+                <i class="far fa-check-circle"></i> Asesoría y capacitación en la implementación de las normas contables (NIF e IFRS).<br>
+
+                <i class="far fa-check-circle"></i> Determinación y valuación del crédito mercantil.<br>
+
+                <i class="far fa-check-circle"></i> Estudio y valuación del deterioro de activos.<br>
+
+                <i class="far fa-check-circle"></i> Proyecciones financieras y presupuestos.<br>
+
+                <i class="far fa-check-circle"></i> Asesoria de costos.
+              </p>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="PRECIOS">
+              <h3>PRECIOS DE TRANSFERENCIA</h3>
+              <p>
+                <i class="far fa-check-circle"></i> Estudio de precios de transferencia. <br>
+
+                <i class="far fa-check-circle"></i> Declaración informativa sobre situación fiscal.<br>
+
+                <i class="far fa-check-circle"></i> Revisión de los anexos 16 y 17 asi como del cuestionario de PT.<br>
+
+                <i class="far fa-check-circle"></i> Revisión de la declaración informativa múltiple (DIM) anexo 9 información de operaciones con partes relacionadas residentes en el extranjero.<br>
+
+                <i class="far fa-check-circle"></i> Revisión de las declaraciones que se establece en el articulo 76-A de la LISR.
+              </p>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="VALUACION">
+              <h3>VALUACION DE EMPRESAS</h3>
+              <p>
+                <i class="far fa-check-circle"></i> Valorar transacciones como las fusiones, adquisiciones, desinversiones, recapitalización y recompra de acciones. <br>
+
+                <i class="far fa-check-circle"></i> Desarrollo de garantías con base en el valor del negocio.<br>
+
+                <i class="far fa-check-circle"></i> Medición de la creación de valor para el accionista.<br>
+
+                <i class="far fa-check-circle"></i> Verificar la gestión llevada a cabo por los directivos.<br>
+
+                <i class="far fa-check-circle"></i> Establecer políticas de dividendos.<br>
+
+                <i class="far fa-check-circle"></i> Medición del impacto de las políticas y estrategias de la empresa en la creación y destrucción de valor.<br>
+
+                <i class="far fa-check-circle"></i> Apoyar procesos de arbitraje y pleitos.<br>
+
+                <i class="far fa-check-circle"></i> Utilizar la gestión basada en el valor para revisar y establecer objetivos para las unidades de negocios.
+              </p>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="CONTABLE">
+              <h3>ASESORIA CONTABLE</h3>
+              <p>
+                <i class="far fa-check-circle"></i> Organización y puesta en marcha de la contabilidad.<br>
+
+                <i class="far fa-check-circle"></i> Gestión diaria de los asientos contables, apertura y cierre de ejercicio.<br>
+
+                <i class="far fa-check-circle"></i> Estudio, revisión y actualización de contabilidades.<br>
+
+                <i class="far fa-check-circle"></i> Asesoramiento general en materia contable y sobre obligaciones formales.<br>
+
+                <i class="far fa-check-circle"></i> Conciliaciones bancarias.<br>
+
+                <i class="far fa-check-circle"></i> Amortizaciones y provisiones.<br>
+
+                <i class="far fa-check-circle"></i> Control de operaciones de leasing, confirming, factoring, renting, etc.<br>
+
+                <i class="far fa-check-circle"></i> Elaboración de Balances de Situación, de Explotación, Cuenta de Pérdidas y Ganancias, Sumas y Saldos.<br>
+
+                <i class="far fa-check-circle"></i> Legalización de libros contables.<br>
+
+                <i class="far fa-check-circle"></i> Preparación y tramitación del depósito de las Cuentas Anuales.<br>
+
+                <i class="far fa-check-circle"></i> Emisión de informes contables.
+              </p>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="TRABAJO">
+              <h3>ASESORIA DE TRABAJO</h3>
+              <p>
+                <i class="far fa-check-circle"></i> Consultas de contenido de trabajo.<br>
+                <i class="far fa-check-circle"></i> Tramitación de nómina y seguro social.<br>
+                <i class="far fa-check-circle"></i> Preparación de contratos de trabajo y gestión de estos antes de la administración.<br>
+                <i class="far fa-check-circle"></i> Cartas de despido y liquidación de asentamientos.<br>
+                <i class="far fa-check-circle"></i> Afiliaciones, aceptaciones y cancelaciones de trabajadores en la seguridad social.<br>
+                <i class="far fa-check-circle"></i> Registro de empresas en el seguro social y modificaciones posteriores (cambio de directores, nuevos códigos contables).<br>
+                <i class="far fa-check-circle"></i> Aceptaciones y cancelaciones en el régimen autónomo y en otros esquemas especiales.<br>
+                
+                <i class="far fa-check-circle"></i> Requerimientos de procesamiento de datos y desempeño, subsidios y subsidios.<br>
+                
+                <i class="far fa-check-circle"></i> Retención de certificados y comunicaciones de datos al pagador.<br>
+                <i class="far fa-check-circle"></i> Representaciones antes de la inspección del trabajo.<br>
+                
+                <i class="far fa-check-circle"></i> Asistencia legal:<br>
+                <i class="far fa-check-circle"></i> Solicitudes al Tribunal Laboral.<br>
+                <i class="far fa-check-circle"></i> Recurso por trabajo y seguridad social.<br>
+                <i class="far fa-check-circle"></i> Preparación y presentación de informes sobre accidentes y enfermedades.<br>
+                <i class="far fa-check-circle"></i> Preparación y presentación de descargos y escritos de oposición contra actos y sanción de autoridades laborales.<br>
+                <i class="far fa-check-circle"></i> Preparación y presentación de solicitudes de proyecciones de pago a la seguridad social, así como la distribución de comisiones y splits.<br>
+                <i class="far fa-check-circle"></i> Legalización del libro de visitas.<br>
+                <i class="far fa-check-circle"></i> Aceptación de hipotecas.<br>
+                <i class="far fa-check-circle"></i> Controles de trabajo.<br>
+                <i class="far fa-check-circle"></i> Consejos para la prevención de riesgos laborales y la salud y la seguridad en el trabajo.<br>
+                <i class="far fa-check-circle"></i> Redacción anual del calendario de trabajo
+              </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+
+<section class="seccion3" id="CONTACTO" style="padding: 5% 0% 5% 0%;">
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center" style="color: #1D1D3C;"><i class="fas fa-phone-volume"></i> CONTACTO</h1> <br><br>
+
+<div class="row">
+<div class="col-sm-4">
+<h3>Datos de la empresa</h3>
+<hr>
+<address>
+<strong>Correo:</strong> <a href="mailto:globalsolutionfcr.com"> info@globalsolutionfcr.com</a><br><br>
+<strong>Teléfono:</strong> PENDIENTE<br><br>
+<strong>Horario:</strong>  Lunes a Viernes de 9 am a 6 pm<br><br>
+<strong>Dirección:</strong> C. 10A #203 POR 1H Y 0 COL. MEXICO NORTE, MERIDA, YUCATAN. C.P. 97128
+</address>
+</div>
+    
+<div class="col-sm-8 contact-form">
+<form id="contact" method="post" class="form" role="form">
+<div class="row">
+<div class="col-xs-6 col-md-4 form-group">
+<input class="form-control" id="name" name="name" placeholder="Nombre" type="text" required  />
+</div>
+<div class="col-xs-6 col-md-4 form-group">
+<input class="form-control" id="name" name="name" placeholder="Teléfono" type="text" required  />
+</div>
+<div class="col-xs-6 col-md-4 form-group">
+<input class="form-control" id="email" name="email" placeholder="Correo" type="email" required />
+</div>
+</div>
+<textarea class="form-control" id="message" name="message" placeholder="Comentario" rows="5"></textarea>
+<br />
+<div class="row">
+<div class="col-xs-12 col-md-12 form-group">
+<button class="btn btn-primary pull-right" type="submit">Enviar</button>
+</form>
+</div>
+</div>
+
+  </div>
+
+</section>
+
+
+<footer>
+<p>Derechos de autor 2018 © | GLOBAL SOLUTION FCR SA DE CV</p>
+</footer>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/sendCORREO.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
